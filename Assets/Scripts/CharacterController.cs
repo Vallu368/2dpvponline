af7ce1863da11183899d483f008dc53c8fa6e3cb
vallu368 <vsavolainen22@gmail.com>
2024-02-19 22:16:52 +0200

    TextMeshPro Network WIP

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        Vector2 movement = new Vector2(moveX, moveY).normalized * speed;
+        rb.velocity = movement;
+    }
+}
